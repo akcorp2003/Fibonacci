2359d63e1bbd032e1cb182fc6b6ac0f4a9949ecf
Aland Kuang <kuanga2016@ucla.edu>
2015-07-14 17:36:47 -0700

    Added Files
    Added files for Fibonacci generator

---
+++ Fibonacci/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumTerms = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TermNum = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Val = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.TermNum, Me.Val})
+        Me.ListView1.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListView1.Location = New System.Drawing.Point(98, 119)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(124, 203)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(56, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(209, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter number of Fibonacci terms to display:"
+        '
+        'NumTerms
+        '
+        Me.NumTerms.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NumTerms.Location = New System.Drawing.Point(98, 39)
+        Me.NumTerms.Name = "NumTerms"
+        Me.NumTerms.Size = New System.Drawing.Size(124, 23)
+        Me.NumTerms.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(98, 74)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(124, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Go!"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TermNum
+        '
+        Me.TermNum.Text = "n"
+        Me.TermNum.Width = 30
+        '
+        'Val
+        '
+        Me.Val.Text = "Term Value"
+        Me.Val.Width = 76
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(321, 351)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.NumTerms)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Name = "Form1"
+        Me.Text = "Fibonacci Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents NumTerms As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TermNum As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Val As System.Windows.Forms.ColumnHeader
+
+End Class
---
+++ Fibonacci/Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Collections
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim myqueue As New Queue(Of Integer)
+        ClearList()
+        myqueue = GetFibonacciTerms(Convert.ToInt32(NumTerms.Text))
+        FillinList(myqueue)
+
+    End Sub
+
+    Public Function GetFibonacciTerms(ByVal num_terms As Integer) As Queue(Of Integer)
+        Dim first_term As Integer = 0
+        Dim second_term As Integer = 1
+
+        Dim fib_queue As New Queue(Of Integer)
+        fib_queue.Enqueue(first_term)
+        fib_queue.Enqueue(second_term)
+
+        Dim total_terms As Integer = num_terms - 2
+        For i As Integer = 0 To total_terms - 1 Step 1
+            Dim curr_term As Integer = first_term + second_term
+            fib_queue.Enqueue(curr_term)
+            first_term = second_term
+            second_term = curr_term
+        Next
+
+        Return fib_queue
+    End Function
+
+    Public Sub FillinList(ByVal listofterms As Queue(Of Integer))
+        Dim val As ListViewItem
+        For i As Integer = 0 To listofterms.Count - 1 Step 1
+            val = ListView1.Items.Add(Convert.ToString(i))
+            val.SubItems.Add(listofterms.Dequeue())
+            ListView1.Update()
+            ListView1.EndUpdate()
+        Next
+    End Sub
+
+    Public Sub ClearList()
+        Dim listitem As ListViewItem
+        If ListView1.Items.Count = 0 Then
+            Return
+        End If
+        For i = ListView1.Items.Count - 1 To 0 Step -1
+            listitem = ListView1.Items(i)
+            ListView1.Items.Remove(listitem)
+        Next
+    End Sub
+End Class
